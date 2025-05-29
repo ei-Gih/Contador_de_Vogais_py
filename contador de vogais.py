7c2faf15af059f8e9b625c90993c124057530ab0
Gisele Lais <105565774+ei-Gih@users.noreply.github.com>
2025-05-29 16:42:39 -0300

    upgrade consoantes

--- contador de vogais.py
+++ contador de vogais.py
@@ -1,25 +1,36 @@
 
 def conta_vogais(texto):
-# TODO: Defina um conjunto de vogais tanto minúsculas quanto maiúsculas:
+# Defina um conjunto de vogais tanto minúsculas quanto maiúsculas:
     vogais = set('aeiouAEIOU')
     
-# TODO: Inicialize um contador para contar as vogais:
+# Inicialize um contador para contar as vogais:
     contador = 0
 
 # Iteramos pelos caracteres da string
     for char in texto:
-# TODO: Verifique se o caractere atual é uma vogal e incremente o valor do contador:
+# Verifique se o caractere atual é uma vogal e incremente o valor do contador:
         if char in vogais:
             contador += 1
     
     return contador
+def conta_consoante(texto):
+    consoantes= set('bBcCçÇdddDfFgGhHjJkKlLmMnNpPqQrRsStTvVwWxXyYzZ')
+    contador = 0
+
+    for char in texto:
+        if char in consoantes:
+            contador += 1
+    return contador
+
 while True:
     # Solicitamos ao usuário que insira uma string
     texto = input("Digite a Palavra: ")
 
-    # Chamamos a função conta_vogais e exibimos o resultado
-    resultado = conta_vogais(texto)
-    print(f"O número de vogais na string '{texto}' é: {resultado}")
+    # Chamamos a função e exibimos o resultado
+    resultado_v = conta_vogais(texto)
+    resultado_c = conta_consoante(texto)
+    print(f"O número de vogais na palavra são: {resultado_v}")
+    print(f"O número de consoantes na palavra são: {resultado_c}")
 
     continuar = input("Deseja Continuar? (s/n):  ")
     
